7b03cb63f0458f97184dc9f0e8ba1da1589bc4b9
Livingstone Ackah <kofiackah360@gmail.com>
2024-05-20 14:25:58 +0000

    Fix all bugs in the code

--- server/models/User.js
+++ server/models/User.js
@@ -1,15 +1,16 @@
+const { uniqueId } = require("lodash");
 const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 const UserSchema = new Schema({
   username: {
     type: String,
-    required: true,
+    require: true,
     unique: true,
   },
   password: {
     type: String,
-    required: true,
+    require: true,
   },
 });
 
--- server/routes/admin.js
+++ server/routes/admin.js
@@ -5,29 +5,29 @@ const User = require("../models/User");
 
 const adminLayout = "../views/layouts/admin";
 
-// Get - Amdin ~ Login Page
+// Get ~ Admin ~ Login Page
 router.get("/admin", async (req, res) => {
   try {
     const locals = {
       title: "Admin",
       description: "A simple blog made with NodeJS, ExpressJS and MongoDB",
     };
-
     res.render("admin/index", { locals, layout: adminLayout });
   } catch (error) {
     console.log(error);
+    console.log("An Error");
   }
 });
 
-// Post - Admin ~ Check Login
-router.post("/admin", async (res, req) => {
+router.post("/admin", async (req, res) => {
   try {
     const { username, password } = req.body;
     console.log(req.body);
+
     res.redirect("/admin");
-    // res.render("admin/index", { locals, layout: adminLayout });
   } catch (error) {
-    console.log("this is an error");
+    console.log(error);
+    console.log("An Error 2");
   }
 });
 
