68b62b201dd66a2d14ff62edf21113cf47d1c5e9
Livingstone Ackah <kofiackah360@gmail.com>
2024-05-21 23:41:59 +0000

    Add register to adminJS

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ require("dotenv").config();
 
 const express = require("express");
 const expressLayouts = require("express-ejs-layouts");
+const cookiePaser = require("cookie-parser");
+const MongoStore = require("connect-mongo");
+const session = require("express-session");
+
 const connectDB = require("./server/config/db");
 
 const app = express();
@@ -10,8 +14,20 @@ const PORT = 3003 || process.env.PORT;
 // Checking if I have connect to the DB
 connectDB();
 
+// Using the middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cookiePaser());
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: true,
+    store: MongoStore.create({
+      mongoUrl: process.env.MONGOBD_URI,
+    }),
+  })
+);
 
 // Static files
 app.use(express.static("public"));
--- server/routes/admin.js
+++ server/routes/admin.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const Post = require("../models/Post");
 const User = require("../models/User");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const adminLayout = "../views/layouts/admin";
 
@@ -19,6 +21,7 @@ router.get("/admin", async (req, res) => {
   }
 });
 
+// Post ~ Admin ~ Check Login
 router.post("/admin", async (req, res) => {
   try {
     const { username, password } = req.body;
@@ -28,6 +31,27 @@ router.post("/admin", async (req, res) => {
   } catch (error) {
     console.log(error);
     console.log("An Error 2");
+  }
+});
+
+// Post ~ Admin ~ register
+router.post("/register", async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    try {
+      const user = await User.create({ username, password: hashedPassword });
+      res.status(201).json({ message: "User Created", user });
+    } catch (error) {
+      if (error.code === 11000) {
+        res.status(409).json({ message: "User already created" });
+      }
+      res.status(500).json({ message: "Internal server erro" });
+    }
+  } catch (error) {
+    console.log(error);
+    console.log("An Error In Register");
   }
 });
 
